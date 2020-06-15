4f07e2bce3eb84d1ccd80dbce1329a6f37ed2d2f
Ryan Palmer <ryangpalmer@hotmail.com>
2020-06-15 06:41:49 -0700

    Simplified player prefab. Player animation is now driven by movement speed.

---
+++ Assets/AnimationSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationSpeed : MonoBehaviour {
+	[SerializeField]
+	private Animator animator;
+
+	[SerializeField]
+	private NavMeshAgent navMeshAgent;
+
+	[SerializeField]
+	private string speedVariableName;
+
+	void Update() {
+		animator.SetFloat(speedVariableName, getNormalizedSpeed());
+	}
+
+	private float getNormalizedSpeed() {
+		float normalizedSpeed = navMeshAgent.velocity.magnitude / navMeshAgent.speed;
+		return Mathf.Clamp(normalizedSpeed, 0, 1);
+	}
+}
